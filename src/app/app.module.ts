b2a53ce3adf89a4a3e0d44a5348e98b38d28c8bc
tcampbe <tcampb@icloud.com>
2020-01-12 21:21:33 -0600

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarouselComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
